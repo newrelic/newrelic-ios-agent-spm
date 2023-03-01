1a7ff8ecd093b6ce588672a8c77085f05027fdd1
runner <runner@Mac-1677693207015.local>
2023-03-01 18:01:05 +0000

    Added build 7.4.3

--- Package.swift
+++ Package.swift
@@ -18,8 +18,8 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .binaryTarget(name: "NewRelic",
-                      url: "https://download.newrelic.com/ios_agent/NewRelic_XCFramework_Agent_7.4.2.zip",
-                      checksum: "23bbb5e3c499e16bc46e11f4a21a11379b31c0097573fe047136d5d02fa7d152")
+                      url: "https://download.newrelic.com/ios_agent/NewRelic_XCFramework_Agent_7.4.3.zip",
+                      checksum: "cfdcacbfc94be358fb4e05d891a28eba40dcbb2bee7ec77845b29f21d1643258")
     ]
 )
 
