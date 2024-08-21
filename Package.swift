10764844ceac73e00d717e93aed31e8bb7c7ec23
Anka <runner@Mac-1724198594700.local>
2024-08-21 00:12:41 +0000

    Added build 7.5.1

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "NewRelic",
     platforms: [
-        .iOS(.v9), .macOS(.v10_14)
+        .iOS(.v9), .macOS(.v10_14), .tvOS(.v9), .watchOS(.v10)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
@@ -19,8 +19,8 @@ let package = Package(
             name: "NewRelicPackage",
             dependencies: []),
         .binaryTarget(name: "NewRelic",
-                      url: "https://download.newrelic.com/ios_agent/NewRelic_XCFramework_Agent_7.5.0.zip",
-                      checksum: "b77e825f9a866f53ed8dd17c435df9ae694223af35e2eaabd6598b38b643b8a4")
+                      url: "https://download.newrelic.com/ios_agent/NewRelic_XCFramework_Agent_7.5.1.zip",
+                      checksum: "0fb9fed8122901bcb3542a2a2f07967a03ca9671b06632fa3f87c49cfd4186a2")
     ]
 )
 
