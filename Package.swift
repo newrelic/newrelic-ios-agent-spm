5db4ebdd71dcefab4719f8a049ac50dc3721759b
Anka <runner@sjc22-bm210-7584bd73-7379-4345-8643-64a401616cfe-D6093863BD8E.local>
2026-07-02 18:35:40 +0000

    Added build 7.7.3

--- Package.swift
+++ Package.swift
@@ -19,8 +19,8 @@ let package = Package(
             name: "NewRelicPackage",
             dependencies: []),
         .binaryTarget(name: "NewRelic",
-                      url: "https://download.newrelic.com/ios_agent/NewRelic_XCFramework_Agent_7.7.2.zip",
-                      checksum: "104c294b4212722c99e18bb974ac2b291e6a5193bd78913254a12d3832acb380")
+                      url: "https://download.newrelic.com/ios_agent/NewRelic_XCFramework_Agent_7.7.3.zip",
+                      checksum: "936256214795c5b21e3cea9c7e5823fcb1cea132cb5860629dfd4314a99a6d08")
     ]
 )
 
